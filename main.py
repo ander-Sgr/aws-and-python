7e11e9692d13f972286632850b37567294226df9
ander-Sgr <andersonbarreto2001@gmail.com>
2024-07-14 08:21:25 +0200

    refactor code for get more info about the instance

--- main.py
+++ main.py
@@ -7,20 +7,44 @@ from argparse import ArgumentParser, Namespace
 from boto3 import ec2
 
 
+class EC2InstanceParams:
+    def __init__(self, name_instance: str, ami_id: str, key_name: str,
+                 security_group_name: str, type_instance: str):
+        self.name_instance = name_instance
+        self.ami_id = ami_id
+        self.key_name = key_name
+        self.type_instance = type_instance
+        self.security_group_name = security_group_name
+        # self.region_name = region_name
+
+
 def parser_args(args: List) -> Namespace:
     parser = ArgumentParser("AWS EC2: Create and manage EC2 instances")
-    parser.add_argument('-c', '--create', action='store_true', help='Create a new EC2 instances')
-    parser.add_argument('-n', '--name', type=str, help='Name of an existing EC2 instance')
-    parser.add_argument('-a', '--ami-id', type=str, help='Id of the Amazon machine')
-    parser.add_argument('-k', '--key-name', type=str, help="Name of the key pair for SSH access")
-    parser.add_argument('-r', '--region', type=str, help="The region of the instance EC2")
-    parser.add_argument('-l', '--list', action='store_true', help='List all EC2 instances')
-    parser.add_argument('-d', '--destroy', action='store_true', help='Destroy an existing EC2 instance')
+    parser.add_argument('-c', '--create', action='store_true',
+                        help='Create a new EC2 instances')
+    parser.add_argument('-n', '--name', type=str,
+                        help='Name for assign to EC2 instance')
+    parser.add_argument('-a', '--ami-id', type=str,
+                        help='Id of the Amazon machine')
+    parser.add_argument('-k', '--key-name', type=str,
+                        help="Name of the key pair for SSH access")
+    # parser.add_argument('-r', '--region', type=str,
+    #                   help="The region of the instance EC2")
+    parser.add_argument('-t', '--type-instance', type=str, default='t2.micro',
+                        help="Type of instance")
+    parser.add_argument('-l', '--list', action='store_true',
+                        help='List all EC2 instances')
+    parser.add_argument('-d', '--destroy', action='store_true',
+                        help='Destroy an existing EC2 instance')
     return parser.parse_args(args)
 
 
-def create_ec2_client():
-    return boto3.client('ec2', region_name='us-east-1')
+def create_ec2_client(region_name: str):
+    return boto3.client('ec2', region_name=region_name)
+
+
+def create_ec2_resource(region_name: str):
+    return boto3.resource('ec2', region_name=region_name)
 
 
 def create_security_group(ec2_client, group_name: str) -> str:
@@ -46,34 +70,18 @@ def create_security_group(ec2_client, group_name: str) -> str:
     return security_group_id
 
 
-def create_ec2_instance(ec2_client, name_instance: str, ami_id: str, key_name: str, region_name: str):
-    ec2_resource = boto3.resource('ec2', region_name)
-    response = ec2_resource.create_instances(
-        ImageId=ami_id,
-        MinCount=1,
-        MaxCount=1,
-        InstanceType='t2.micro',
-        KeyName=key_name,
-        SecurityGroups=[create_security_group(ec2_client)],
-        TagSpecifications=[
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': name_instance
-                    }
-                ]
-            }
-        ]
-    )[0]
-    response.wait_until_running()
-    response.reload()
-
-    return response.id, response.public_ip_address
+def init_instance_params(parsed_args: Namespace) -> EC2InstanceParams:
+    return EC2InstanceParams(
+        name_instance=parsed_args.name,
+        ami_id=parsed_args.ami_id,
+        key_name=parsed_args.key_name,
+        type_instance=parsed_args.type_instance,
+        security_group_name='SSHAccess',
+        # region_name=parsed_args.region
+    )
 
 
-def exist_security_group(ec2_client, group_name: str, region_name: str) -> bool:
+def exist_security_group(ec2_client, group_name: str) -> bool:
     response = ec2_client.describe_security_groups(
         Filters=[
             {"Name": "group-name", "Values": [group_name]},
@@ -85,16 +93,53 @@ def exist_security_group(ec2_client, group_name: str, region_name: str) -> bool:
         return False
 
 
+def create_ec2_instance(ec2_client, ec2_resource, params: EC2InstanceParams):
+    if not exist_security_group(ec2_client, params.security_group_name):
+        security_group_id = create_security_group(ec2_client, params.security_group_name)
+        print(f"Created security group with ID: {security_group_id}")
+
+    response = ec2_resource.create_instances(
+        ImageId=params.ami_id,
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.micro',
+        KeyName=params.key_name,
+        SecurityGroups=[params.security_group_name],
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': params.name_instance
+                    }
+                ]
+            }
+        ]
+    )[0]
+    response.wait_until_running()
+    response.reload()
+
+    return response.id, response.public_ip_address, response.instance_type
+
+
 def main(args: list):
     parsed_args = parser_args(args)
-    ec2_client = create_ec2_client()
+    ec2_client = create_ec2_client('us-east-1')
+    ec2_resource = create_ec2_resource('us-east-1')
 
-    if (parsed_args.create and parsed_args.name and parsed_args.ami_id
-            and parsed_args.key_name) and parsed_args.region:
-        security_group_id = create_security_group(ec2_client)
-        print(f"Created security group with ID: {security_group_id}")
-        instance_id, public_ip = create_ec2_instance(ec2_client, parsed_args.name,
-                                                     parsed_args.ami_id, parsed_args.key_name, parsed_args.region)
+    if parsed_args.create and parsed_args.name and parsed_args.ami_id \
+            and parsed_args.key_name and parsed_args.type_instance:
+        params = EC2InstanceParams(
+            name_instance=parsed_args.name,
+            ami_id=parsed_args.ami_id,
+            key_name=parsed_args.key_name,
+            security_group_name='SSHAccess-test',
+            type_instance=parsed_args.type_instance
+        )
+        instance_id, public_ip, instance_type = create_ec2_instance(ec2_client, ec2_resource, params)
+        print(f"Created EC2 instance with ID: {instance_id} Public IP: {public_ip} "
+              f" Type of instance: {instance_type}")
     else:
         print("Invalid input: Use -h or --hel for see the instructions ")
 
